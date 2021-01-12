c28518f543132c3b7b90cb4c3cd177c38d3ae129
sam33na <saminashrestha98@gmail.com>
2021-01-12 23:12:15 +0545

    register design/bind

--- app/src/main/java/com/sr/studentregister/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/sr/studentregister/ui/register/RegisterFragment.kt
@@ -10,12 +10,49 @@ import com.sr.studentregister.Model.Students
 import com.sr.studentregister.R
 
 class RegisterFragment : Fragment() {
+    private lateinit var etName: EditText
+    private lateinit var etAge: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var radGrp: RadioGroup
+    private lateinit var male: RadioButton
+    private lateinit var female: RadioButton
+    private lateinit var others: RadioButton
+    private lateinit var btnSub: Button
+    var gender :String = "Female";
+    var list = arrayListOf<Students>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
+        etName=view.findViewById(R.id.etName)
+        etAddress=view.findViewById(R.id.etAddress)
+        etAge=view.findViewById(R.id.etAge)
+        radGrp=view.findViewById(R.id.radioGrp)
+        male=view.findViewById(R.id.radMale)
+        female=view.findViewById(R.id.radFemale)
+        others=view.findViewById(R.id.radOther)
+        btnSub=view.findViewById(R.id.btnSubmit)
+        male.setOnClickListener {
+            gender="Male"
+        }
+        female.setOnClickListener {
+            gender="Female"
+        }
+        others.setOnClickListener {
+            gender="Others"
+        }
+
+        btnSub.setOnClickListener {
+            var name= etName.text.toString()
+            var address= etAddress.text.toString()
+            var age= etAge.text.toString().toInt()
+            var Student=Students(name, address, age, gender)
+            list.add(Student)
+            Toast.makeText(context,"Student added", Toast.LENGTH_LONG).show()
+        }
         return view
     }
 }
