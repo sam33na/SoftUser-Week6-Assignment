c2e2a66b5c60a187bc2afedd65ccbdf706d11797
sam33na <saminashrestha98@gmail.com>
2021-01-12 23:36:50 +0545

    student adapter (unfinished)

---
+++ app/src/main/java/com/sr/studentregister/Adapter/StudentAdapter.kt
@@ -0,0 +1,58 @@
+package com.sr.studentregister.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sr.studentregister.Model.Students
+import com.sr.studentregister.R
+
+class StudentAdapter(
+    val lstStd:ArrayList<Students>,
+    val context:Context
+):RecyclerView.Adapter<StudentAdapter.StudentViewHolder>()
+{
+    class StudentViewHolder(view: View):RecyclerView.ViewHolder(view)
+    {
+        val imgDp:ImageView
+        val imgDel:ImageView
+        val tvName:TextView
+        val tvAge: TextView
+        val tvAddress: TextView
+        val tvGender:TextView
+
+        init {
+            imgDp=view.findViewById(R.id.imgDp)
+            imgDel=view.findViewById(R.id.imgDel)
+            tvName=view.findViewById(R.id.tvName)
+            tvAge=view.findViewById(R.id.tvAge)
+            tvAddress=view.findViewById(R.id.tvAddress)
+            tvGender=view.findViewById(R.id.tvGender)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_layout,parent,false)
+        return StudentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val std = lstStd[position]
+        holder.tvName.text=std.stdName
+        holder.tvAddress.text=std.stdAddress
+        holder.tvAge.text=std.stdAge.toString()
+        holder.tvGender.text=std.stdGender
+
+        Glide.with(context)
+            .load(std.stdDpUrl)
+            .into(holder.imgDp)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/sr/studentregister/Model/Students.kt
+++ app/src/main/java/com/sr/studentregister/Model/Students.kt
@@ -1,6 +1,7 @@
 package com.sr.studentregister.Model
 
 data class Students (
+    val stdDpUrl:String?=null,
     val stdName:String?=null,
     val stdAddress:String?=null,
     val stdAge:Int?=null,
