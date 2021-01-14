5b81ba1d2cbba85d8d7827b6edf9b599aa952dc9
sam33na <saminashrestha98@gmail.com>
2021-01-14 19:41:18 +0545

    web view

--- app/src/main/java/com/sr/studentregister/ui/about/AboutFragment.kt
+++ app/src/main/java/com/sr/studentregister/ui/about/AboutFragment.kt
@@ -17,7 +17,14 @@ private lateinit var wbView: WebView
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)\
+        wbView= view.findViewById(R.id.wbView)
+        wbView.loadUrl("https://softwarica.edu.np/")
+
+        val WebSettings = wbView.getSettings();
+        WebSettings.setJavaScriptEnabled(true)
+
+        wbView.setWebViewClient(WebViewClient())
         return view
     }
 }
